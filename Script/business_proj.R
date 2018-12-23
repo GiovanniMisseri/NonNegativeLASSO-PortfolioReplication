26597a6a824615d53b62ac956fa58554d2c2e80e
GiovanniMisseri <Giovanni.misseri@gmail.com>
2018-12-23 16:40:49 +0100

    dataset time series coefficients

--- Script/business_proj.R
+++ Script/business_proj.R
@@ -1,34 +1,35 @@
-library(readxl)
-dat <- read_excel("C:/Users/luigimissri/Desktop/FILE DA SALVARE/Data Science/II anno/Business economics and financial data/Project/SP500_10feb2017_components.xlsx", 
-                                         sheet = "SPX Components", skip = 2)
-library(readxl)
-dat1 <- read_excel("C:/Users/luigimissri/Desktop/FILE DA SALVARE/Data Science/II anno/Business economics and financial data/Project/SP500_10feb2017_components.xlsx", 
-                                         sheet = "SPX Index", skip = 2)
-names=read_excel("C:/Users/luigimissri/Desktop/FILE DA SALVARE/Data Science/II anno/Business economics and financial data/Project/SP500_10feb2017_components.xlsx", 
-                 sheet = "Names",col_names = F)
-
-tot=merge(dat1,dat,by = "CURRENCY")
-colnames(tot)=names[1,]
-
-write.csv(dldat,"dldat.csv")
-
-dat=dat_tot[10700:11508,-1]
-
-lodat=log(dat)
-dldat=lodat[2:809,]-lodat[1:808,]
-
-
-
 # see https://web.stanford.edu/~hastie/glmnet/glmnet_alpha.html to learn more
+dldat <- read.csv("~/GitHub/NonNegativeLASSO-PortfolioReplication/Data/dldat.csv", row.names=1)
 require(glmnet)
-a=colSums(is.na(dldat[,-1]))
+a=colSums(is.na(dldat))
 sum(a!=0)
+dldat=dldat[,a==0]
+
+
 x=dldat[,-1]
-x=x[,a==0]
 x=as.matrix(x)
-y=dldat[,1]
-summary(y)
-mod=glmnet(x ,y )
-mod
-summary(mod)
-mod$beta
+y=(dldat[,1])
+
+
+mod=cv.glmnet(x ,y,lower.limits=0 )
+
+plot(mod)
+mod$lambda.1se
+sum(coef(mod,s=0.0001)==0)
+sum(coef(mod,s=0.0001)!=0)
+dim(coef(mod,s=0.0001))
+
+beta_time=matrix(NA,494,807)
+
+i=1
+beta_time[,1]=(coef(mod,s=0.001)[,1])
+sum(beta_time[,1]==0)
+for( i in 1:558){
+  x=as.matrix(dldat[i:(i+249),-1])
+  y=as.matrix(dldat[i:(i+249),1])
+  mod=cv.glmnet(x ,y,lower.limits=0 )
+  beta_time[,i]=(coef(mod,s=0.001)[,1])
+
+}
+
+#write.csv(beta_time,"ts_beta.csv")
